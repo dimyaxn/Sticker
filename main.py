94386654ad2d0a07a17d9cd0b683d72867199f09
Dmitry Starichenko <dimyaxn@yandex.ru>
2023-09-28 13:17:54 +0700

    Always Top

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide6 import QtCore
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from design import Ui_MainWindow
+
+
+
+class Sticker(QMainWindow):
+    def __init__(self):
+        super(Sticker, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Sticker()
+    window.setWindowFlag(QtCore.Qt.WindowStaysOnTopHint)
+    window.show()
+    sys.exit(app.exec())
